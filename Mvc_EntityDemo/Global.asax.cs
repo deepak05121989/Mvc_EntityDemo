ea9fe8d11836678c1a8854f889e05ad71c5f02bf
Acer <mailtodeepakchoudhary@gmail.com>
2024-02-24 10:33:20 +0530

    Add project files.

---
+++ Mvc_EntityDemo/Global.asax.cs
@@ -0,0 +1,14 @@
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Mvc_EntityDemo
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
